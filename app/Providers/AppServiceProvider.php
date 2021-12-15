3649bdf6c2e8a8b2a102c134a423768d8a4002c9
Riabukhin Sergei <spywsamara@gmail.com>
2021-12-16 01:12:35 +0400

    Register eloquent search as search service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Articles\EloquentSearchRepository;
+use App\Articles\SearchRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            SearchRepository::class,
+            EloquentSearchRepository::class
+        );
     }
 
     /**
