9f157261f33de9ea626b806ea3cff1b355ed639c
Riabukhin Sergei <spywsamara@gmail.com>
2021-12-16 00:41:26 +0400

    Configure article seeder

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Article;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class ArticleFactory extends Factory
 {
+    protected $model = Article::class;
+
     /**
      * Define the model's default state.
      *
@@ -13,8 +16,21 @@ class ArticleFactory extends Factory
      */
     public function definition()
     {
+        $tags = collect([
+            'my',
+            'your',
+            'some',
+            'more',
+            'data',
+            'export',
+            'anything',
+            'owner',
+        ]);
+
         return [
-            //
+            'title' => $this->faker->sentence(),
+            'body'  => $this->faker->text(),
+            'tags'  => $tags->random(2)->values()->all(),
         ];
     }
 }
