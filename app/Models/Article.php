766dad9947ee789dc435439f77fd7aec5881fc90
Riabukhin Sergei <spywsamara@gmail.com>
2021-12-16 01:49:38 +0400

    Add searchable trait to article model
    Implement 'toElasticsearchDocumentArray' method for indexing.

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,14 +2,24 @@
 
 namespace App\Models;
 
+use App\Search\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $casts = [
         'tags' => 'json',
     ];
+
+    public function toElasticsearchDocumentArray(): array
+    {
+        return [
+            'title' => $this->title(),
+            'body'  => $this->body(),
+        ];
+    }
 }
